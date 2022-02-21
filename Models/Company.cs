b6c3e390bd7e533ba0873c8ca99f13a07dd5b99b
SamanthaDison <samnicdis@gmail.com>
2022-02-20 20:48:33 -0700

    first commit

---
+++ Models/Company.cs
@@ -0,0 +1,11 @@
+
+
+namespace csharp_contract.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/Contractor.cs
@@ -0,0 +1,10 @@
+
+namespace csharp_contracted.Models
+{
+    public class Contractor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
